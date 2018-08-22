7dcb090bcb6ee96ad798f7b58cf6567bdcaf7c27
Vojta <kozak.v@gmail.com>
2018-08-22 22:23:36 +0200

    Refactor to use promises

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,25 +5,25 @@ const it = require("mocha").it;
 const assert = require("assert");
 
 
-describe("IP Tools client", () => {
+describe("IP Tools client", async () => {
     it("should retrieve information about an IP address", async () => {
-        const IpTools = require("./../index");
-        const ipTools = new IpTools.client();
+            const IpTools = require("./../index");
+            const ipTools = new IpTools.client();
 
-        const geo = IpTools.modules.geo;
-        const asn = IpTools.modules.asn;
-        const dns = IpTools.modules.dns;
+            const geo = IpTools.modules.geo;
+            const asn = IpTools.modules.asn;
+            const dns = IpTools.modules.dns;
 
-        ipTools
-            .use(asn)
-            .use(dns)
-            .use(geo);
+            ipTools
+                .use(asn)
+                .use(dns)
+                .use(geo);
 
-        ipTools.go("109.183.50.244", {}, (ip, data) => {
+            const data = await ipTools.run("109.183.50.244");
+
             assert.strictEqual(data.asn.countryCode, "CZ");
             assert.strictEqual(data.dns.hostname, "109-183-50-244.tmcz.cz");
             assert.strictEqual(data.geo.isp, "T-Mobile Czech Republic a.s.");
-        });
         }
     );
 });
